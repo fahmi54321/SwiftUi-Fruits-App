e2b038dc83c924ef1b17c6c4b27f6e3f8b605d48
fahmi <fahmiabdulaziz44@gmail.com>
2023-08-26 14:34:21 +0700

    Initial Commit

---
+++ SwiftUi Fruits/SwiftUi_FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_FruitsApp.swift
+//  SwiftUi Fruits
+//
+//  Created by Fahmi Aziz on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
